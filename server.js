575a41d8200abf93d51ffd3c8d79c088f2401557
Deniss Simonaits <dsimonaits@gmail.com>
2023-02-27 21:26:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongooseConnect = async () => {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(process.env.DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+const start = async () => {
+  try {
+    app.listen(PORT, async () => {
+      await mongooseConnect()
+        .then(async () => {
+          console.log("Database connection successful");
+        })
+        .catch((e) => {
+          console.log("Database connection failed");
+          process.exit(1);
+        });
+    });
+  } catch (error) {
+    console.error(`Failed to launch application with error: ${error.message}`);
+  }
+};
+
+start();
