fbbc964481fae943bb3ceaa53be47ccc9d5bc519
Deniss Simonaits <dsimonaits@gmail.com>
2023-02-28 22:46:28 +0200

    Update create.validator.js

--- middlewares/joi/create.validator.js
+++ middlewares/joi/create.validator.js
@@ -1,5 +1,5 @@
 const Joi = require("joi");
-const { WrongParametersError } = require("../../utils/errors");
+const WrongParametersError = require("../../utils/index");
 
 const schema = Joi.object({
   name: Joi.string()
