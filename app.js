522d85fc3939296123c54f4f70782ec382c5c672
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-06 00:35:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const errorHandler = require("./utils/errorHandler");
 const { checkConnection } = require("./models/connectionMongoDb");
-
-const contactsRouter = require("./routes/api/contacts");
-
+const auth = require("./middlewares/auth");
+const contactsRouter = require("./routes/api/contactsRouter");
+const authRouter = require("./routes/api/authRouter");
+require("./config/config-passport");
+const errorHandler = require("./helpers/errors/errorHandler");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -15,7 +16,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
+app.use("/api/contacts", auth, contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Page not found" });
