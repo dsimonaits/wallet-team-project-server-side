610abcabb7ffd43db81beb9a297d1fe0a4a4e44d
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-06 00:34:50 +0200

    Create errorHandler.js

---
+++ helpers/errors/errorHandler.js
@@ -0,0 +1,31 @@
+const { RequestError } = require("./requestErrors");
+const { AuthError } = require("./authErrors");
+
+const errorHandler = (error, req, res, next) => {
+  if (error instanceof RequestError) {
+    return res.status(error.status).json({
+      status: "error",
+      code: error.status,
+      ResponseBody: { message: error.message },
+    });
+  }
+
+  if (error instanceof AuthError) {
+    return res.status(error.status).json({
+      status: error.statusType,
+      code: error.status,
+      ResponseBody: { message: error.message },
+    });
+  }
+
+  const response = {
+    status: "Internal Server Error",
+    code: 500,
+    message: error.message,
+    ResponseBody: { message: error.message },
+  };
+
+  return res.status(500).json(response);
+};
+
+module.exports = errorHandler;
