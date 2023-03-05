d7d3f5c6b3a94cf0366274c6e508d478fc8c0662
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-06 00:34:26 +0200

    Update contactModel.js

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,25 +1,31 @@
 const mongoose = require("mongoose");
 
-const contactsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      default: "not provided",
+    },
+    phone: {
+      type: String,
+      required: [true, "Set phone number for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    default: "not provided",
-  },
-  phone: {
-    type: String,
-    unique: true,
-    required: [true, "Set phone number for contact"],
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Contacts = mongoose.model("contacts", contactsSchema);
 
