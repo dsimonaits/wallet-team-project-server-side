2176c68ac7c64347e3ab892bf80f812e96da07a5
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-06 00:33:59 +0200

    Delete getContacts.js

--- services/contacts/getContacts.js
+++
@@ -1,9 +0,0 @@
-const Contacts = require("../../models/contactModel");
-
-const getContacts = async () => {
-  const contacts = await Contacts.find();
-
-  return contacts;
-};
-
-module.exports = getContacts;
