0d8ee036aa216425324b97cb18d5c7d52a770ac8
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-15 00:25:49 +0200

    Update index.js

--- services/authServices/index.js
+++ services/authServices/index.js
@@ -2,7 +2,7 @@ const signIn = require("./signIn");
 const register = require("./signUp");
 const logout = require("./logout");
 const current = require("./current");
-const updateSubscriptionStatus = require("./updateAvatar");
+const updateSubscriptionStatus = require("./updateSubscriptionStatus");
 const updateAvatar = require("./updateAvatar");
 
 module.exports = {
