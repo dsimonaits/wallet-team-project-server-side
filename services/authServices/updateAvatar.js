dec6db18025aefe4f36ea38ed5c185b8756d85a8
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-15 00:25:48 +0200

    Update updateAvatar.js

--- services/authServices/updateAvatar.js
+++ services/authServices/updateAvatar.js
@@ -1,20 +1,35 @@
 const Users = require("../../models/userModel");
+const Jimp = require("jimp");
+const { v4: uuidv4 } = require("uuid");
 const { NotFound } = require("../../helpers/errors");
+const rootPath = require("path");
+const fs = require("fs").promises;
+const storeAvatar = rootPath.resolve("./public/avatars/");
 
-const updateAvatar = async (id, avatar) => {
-  const updatedContact = await Users.findByIdAndUpdate(
+const updateAvatar = async (id, path, originalname) => {
+  const [name, extension] = originalname.split(".");
+
+  const avatarNewName = `${name}-${uuidv4()}.${extension}`;
+
+  const newAvatarPath = rootPath.join(storeAvatar, avatarNewName);
+
+  Jimp.read(path, async (err, userAvatar) => {
+    if (err) throw err;
+    userAvatar.resize(250, 250).quality(60).write(newAvatarPath);
+    await fs.unlink(path);
+  });
+
+  const updateAvatarPath = await Users.findByIdAndUpdate(
     { _id: id },
-    { avatarURL: avatar },
+    { avatarURL: newAvatarPath },
     {
       new: true,
     }
-  ).select({ avatarURL: 1, _id: 0 });
+  );
 
-  if (!updatedContact) {
+  if (!updateAvatarPath) {
     throw new NotFound("Not found");
   }
-
-  return updatedContact;
 };
 
 module.exports = updateAvatar;
