a638f984d7baff8d39a4037295efbc81def30000
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-15 00:25:59 +0200

    Update multerUploadMiddleware.js

--- middlewares/multerUploadMiddleware.js
+++ middlewares/multerUploadMiddleware.js
@@ -1,15 +1,14 @@
 const multer = require("multer");
 const path = require("path");
-const { v4: uuidv4 } = require("uuid");
 const uploadDir = path.resolve("./tmp");
-const storeAvatar = path.resolve("./public/avatars");
+const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    const [, extension] = file.originalname.split(".");
+    const [_, extension] = file.originalname.split(".");
     cb(null, `${uuidv4()}.${extension}`);
   },
 });
