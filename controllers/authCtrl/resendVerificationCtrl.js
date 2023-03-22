928a5ee1769bf94bfcc313764f02f62a731a1366
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-23 00:45:29 +0200

    Update resendVerificationCtrl.js

--- controllers/authCtrl/resendVerificationCtrl.js
+++ controllers/authCtrl/resendVerificationCtrl.js
@@ -1,14 +1,10 @@
-const { signUpVerification } = require("../../services/authServices");
+const { resendVerification } = require("../../services/authServices");
 const { responseOk } = require("../../helpers/responses");
 
 const resendVerificationCtrl = async (req, res, next) => {
   const { email } = req.body;
 
-  if (!email) {
-    res.json(responseOk("Success", 200, "Verification email sent"));
-  }
-
-  await signUpVerification(email);
+  await resendVerification(email);
 
   res.json(responseOk("Success", 200, "Verification email sent"));
 };
