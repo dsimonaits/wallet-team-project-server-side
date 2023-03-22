0c4a1eb32bba6ae3102d78f9151ecd103ab27fd0
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-23 00:44:40 +0200

    Update resendVerification.js

--- services/authServices/resendVerification.js
+++ services/authServices/resendVerification.js
@@ -1,7 +1,9 @@
 const User = require("../../models/userModel");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const { v4: uuidv4 } = require("uuid");
 const { Conflict } = require("../../helpers/errors");
+const { WrongParametersError } = require("../../helpers/errors");
 
 const resendVerification = async (email) => {
   const user = await User.findOne({ email });
@@ -9,18 +11,28 @@ const resendVerification = async (email) => {
   if (!user) {
     throw new Conflict("User not found", "conflict");
   }
-  user.verify = true;
-  user.verificationToken = "null";
+
+  if (user.verify === true) {
+    throw new WrongParametersError(
+      "Verification has already been passed",
+      "Bad Request"
+    );
+  }
+
+  const verificationToken = uuidv4();
+
+  user.verificationToken = verificationToken;
 
   await user.save();
 
   const msg = {
-    to: user.email,
+    to: email,
     from: "dsimonaits@gmail.com",
-    subject: "Registration",
-    text: "Congratulations, you have successfully registered",
-    html: "<strong>Congratulations, you have successfully registered</strong>",
+    subject: "Please verify your email",
+    text: verificationRequestEmail(email, verificationToken),
+    html: verificationRequestEmail(email, verificationToken),
   };
+
   sgMail
     .send(msg)
     .then(() => {
