9c13bed52d4e1336b5378c13fbb4d9dbc66d17e2
Soan1488 <andrisokolovskiy@gmail.com>
2023-04-08 01:24:10 +0200

    Сделал схему с заготовкой на даты если не сделаю подщёт по месяцам и годам через агригейт, и с заготовкой под владельца транзакции когда галя закончит доделаю. Сделал создание транзакции(посмотри пожалуйста если норм использую хендлеры ошибок, придумал так но может есть способ получше). Сделал удаление(посмотри если нужно проверять на существование транзакции, я думаю нет)Если что-то не так доработаю)))

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const authRouter = require('./routes/api/auth')
+const { transactionRouter } = require("./routes/api");
+const authRouter = require("./routes/api/auth");
 const { checkConnection } = require("./models/connectionMongoDb");
 const errorHandler = require("./helpers/errors/errorHandler");
 const test = require("./routes/api/test");
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-const cookieParser=require('cookie-parser');
-
+const cookieParser = require("cookie-parser");
 
 app.use(checkConnection);
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/user', authRouter )
+app.use("/api/user", authRouter);
 app.use("/api/test", test);
+app.use("/api/transaction", transactionRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Page not found" });
---
+++ controllers/transaction/index.js
@@ -0,0 +1,7 @@
+const transactionCreateCtrl = require("./transactionCreateCtrl");
+const transactionDeleteCtrl = require("./transactionDeleteCtrl");
+
+module.exports = {
+  transactionCreateCtrl,
+  transactionDeleteCtrl,
+};
