1c89e1830c7ccd57a5166d12ebe5eafef4085b6b
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-08 20:11:43 +0300

    Update testMessageCtrl.js

--- controllers/testCtrl/testMessageCtrl.js
+++ controllers/testCtrl/testMessageCtrl.js
@@ -3,7 +3,7 @@ const testMessage = require("../../services/testMessage/testMessage");
 
 const testMessageCtrl = async (req, res, next) => {
   const { text } = req.body;
-
+  const testDeploy = "Беда, всё пропало"
   const newText = await testMessage(text);
 
   res.json(responseOk("Success", 201, "Text message created", newText));
