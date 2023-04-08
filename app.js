c3856903e7d5a3a056cad4deaf3548aeeed6d9a4
Svetlana Redyukhina <svetikred77@gmail.com>
2023-04-08 18:16:39 +0300

    add login and logout

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const authRouter = require('./routes/api/auth')
+const authRouter = require("./routes/api/auth");
 const { checkConnection } = require("./models/connectionMongoDb");
 const errorHandler = require("./helpers/errors/errorHandler");
 const test = require("./routes/api/test");
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-const cookieParser=require('cookie-parser');
-
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 app.use(checkConnection);
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/user', authRouter )
+require("./config/config-passport");
+
+app.use("/api/user", authRouter);
 app.use("/api/test", test);
 
 app.use((req, res) => {
--- models/userSchema.js
+++ models/userSchema.js
@@ -2,37 +2,49 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name:{
-    type:String,
-     required: [true, "name is required"]
-  },
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+
     password: {
       type: String,
       required: [true, "Password is required"],
     },
+
     email: {
       type: String,
       required: [true, "Email is required"],
       unique: true,
-      index:true, 
+      index: true,
     },
-isActivated:{
-    type: Boolean,
-    default:false
-},
-    activationLink:{
-        type:String,
-    
-  }
-});
-  
-//   userSchema.methods.setPassword = function (password) {
-//     this.password = bcrypt.hashSync(password, bcrypt, bcrypt.genSaltSync(6));
-//   };
-//   userSchema.methods.validPassword = function (password) {
-//     return bcrypt.compareSync(password, this.password);
-//   };
-  
-  const UserSchema = mongoose.model("User", userSchema);
-  module.exports = UserSchema;
+
+    token: {
+      type: String,
+      default: null,
+    },
+
+    isActivated: {
+      type: Boolean,
+      default: false,
+    },
+
+    activationLink: {
+      type: String,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+// userSchema.methods.setPassword = function (password) {
+//   this.password = bcrypt.hashSync(password, bcrypt, bcrypt.genSaltSync(6));
+// };
+
+// userSchema.methods.validPassword = function (password) {
+//   return bcrypt.compareSync(password, this.password);
+// };
+
+const UserSchema = mongoose.model("User", userSchema);
+module.exports = UserSchema;
