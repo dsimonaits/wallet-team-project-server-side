1280945c71876767fefa5eef0c0555e60cc058c8
Svetlana Redyukhina <svetikred77@gmail.com>
2023-04-09 21:30:57 +0300

    fix routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,11 +12,11 @@ const {
 const auth = require("../../middlewares/authMiddlewares");
 
 router.post("/signup", catchAsync(signupController));
-// router.post('/login', login)
-// router.post('/logout',authenticate, logout )
+router.post("/login", catchAsync(loginController));
+router.get("/logout", auth, catchAsync(logoutController));
 // router.get('/users/current',currentUser )
 // router.get('/activate/:link', activate)
-router.get("/:id", catchAsync(getUserById));
+// router.get("/:id", catchAsync(getUserById));
 router.get("/refresh", refreshTokenController);
 router.get("/users", catchAsync(usersController));
 
