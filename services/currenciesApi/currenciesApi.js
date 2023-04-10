88175afc2575a228d6fb17e4202f45f1c544a596
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-10 11:29:57 +0300

    Making request limit once per 5 minutes and seting data to localStorage

--- services/currenciesApi/currenciesApi.js
+++ services/currenciesApi/currenciesApi.js
@@ -1,16 +1,40 @@
-const axios = require('axios');
+const axios = require("axios");
+const pLimit = require("p-limit");
+
+const limit = pLimit({
+  concurrency: 1,
+  interval: 5 * 60 * 1000,
+});
+
+const STORAGE_KEY = "cachedResponse";
 
 const currenciesApi = async () => {
-try {
-    const response = await axios("https://api.monobank.ua/bank/currency")
+  let cachedResponse = null;
 
-    console.log(response)
+  try {
+    const storageResponse = localStorage.getItem(STORAGE_KEY);
+    if (storageResponse) {
+      cachedResponse = JSON.parse(storageResponse);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 
-    return response.data
-} catch (error) {
-    console.log(error.message)
-}
-   
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  await limit();
+
+  try {
+    const response = await axios("https://api.monobank.ua/bank/currency");
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(response.data));
+
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 module.exports = currenciesApi;
