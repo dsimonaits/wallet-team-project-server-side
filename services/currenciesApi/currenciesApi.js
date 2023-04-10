842053ddf0cf7d2fa276e212e21d9ddf6303e0a4
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-10 11:54:58 +0300

    currencies Api other logic plimit works with isues

--- services/currenciesApi/currenciesApi.js
+++ services/currenciesApi/currenciesApi.js
@@ -1,42 +1,82 @@
 const axios = require("axios");
-const pLimit = require("p-limit");
 
-const concurrencyLimit = 1;
-
-const limit = pLimit({
-  concurrency: concurrencyLimit,
-  interval: 300000,
-});
-
-const STORAGE_KEY = "cachedResponse";
+const CURRENCY_CACHE_KEY = "currencyCache";
+const CACHE_TTL = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 const currenciesApi = async () => {
-  let cachedResponse = null;
-
-  try {
-    const storageResponse = localStorage.getItem(STORAGE_KEY);
-    if (storageResponse) {
-      cachedResponse = JSON.parse(storageResponse);
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
+  const cachedResponse = localStorage.getItem(CURRENCY_CACHE_KEY);
 
   if (cachedResponse) {
-    return cachedResponse;
+    const { data, timestamp } = JSON.parse(cachedResponse);
+    const lastRequest = Date.now() - timestamp;
+
+    if (lastRequest < CACHE_TTL) {
+      console.log(`Returning cached response (lastRequest: ${lastRequest}ms)`);
+      return data;
+    }
   }
 
-  await limit();
-
   try {
-    const response = await axios("https://api.monobank.ua/bank/currency");
+    const response = await axios.get("https://api.monobank.ua/bank/currency");
+    const responseData = response.data;
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(response.data));
+    localStorage.setItem(
+      CURRENCY_CACHE_KEY,
+      JSON.stringify({
+        data: responseData,
+        timestamp: Date.now(),
+      })
+    );
 
-    return response.data;
+    console.log(responseData);
+
+    return responseData;
   } catch (error) {
     console.log(error.message);
   }
 };
 
 module.exports = currenciesApi;
+
+// const axios = require("axios");
+// const pLimit = require("p-limit");
+
+// const concurrencyLimit = 1;
+
+// const limit = pLimit({
+//   concurrency: concurrencyLimit,
+//   interval: 300000,
+// });
+
+// const STORAGE_KEY = "cachedResponse";
+
+// const currenciesApi = async () => {
+//   let cachedResponse = null;
+
+//   try {
+//     const storageResponse = localStorage.getItem(STORAGE_KEY);
+//     if (storageResponse) {
+//       cachedResponse = JSON.parse(storageResponse);
+//     }
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+
+//   if (cachedResponse) {
+//     return cachedResponse;
+//   }
+
+//   await limit();
+
+//   try {
+//     const response = await axios("https://api.monobank.ua/bank/currency");
+
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(response.data));
+
+//     return response.data;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// module.exports = currenciesApi;
