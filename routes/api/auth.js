dfea116ef935ff3dd321727811108bc81768f615
Soan1488 <andrisokolovskiy@gmail.com>
2023-04-11 13:38:52 +0200

    fixed import

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,20 +13,19 @@ const {
   currentUserController,
 } = require("../../controllers/auth/index");
 
-const { validationAuth } = require("../../helpers/validation");
+const { validateAuth } = require("../../helpers/validation");
 
 // const auth = require("../../middlewares/authMiddlewares");
-const auth = require('../../middlewares/authMiddleware')
+const auth = require("../../middlewares/authMiddleware");
 
-
-router.post('/signup', catchAsync(signupController));
-router.post("/login", validationAuth, catchAsync(loginController));
-router.post('/logout',auth,catchAsync( logoutController) )
-router.get('/current', auth, catchAsync(currentUserController ))
+router.post("/signup", catchAsync(signupController));
+router.post("/login", validateAuth, catchAsync(loginController));
+router.post("/logout", auth, catchAsync(logoutController));
+router.get("/current", auth, catchAsync(currentUserController));
 // router.get('/activate/:link', activate)
-router.get('/:id',auth, catchAsync(getUserById))
-router.get('/refresh',auth, catchAsync(refreshTokenController))
-router.get('/users', auth,catchAsync(usersController))
+router.get("/:id", auth, catchAsync(getUserById));
+router.get("/refresh", auth, catchAsync(refreshTokenController));
+router.get("/users", auth, catchAsync(usersController));
 
 router.get("/google", catchAsync(googleAuth));
 router.get("/google-redirect", catchAsync(googleRedirect));
