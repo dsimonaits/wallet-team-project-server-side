59d8d2b766a44f57994f12c2c3a1e4d910313cb0
Soan1488 <andrisokolovskiy@gmail.com>
2023-04-16 16:48:44 +0200

    fixed delete

--- controllers/transaction/transactionDeleteCtrl.js
+++ controllers/transaction/transactionDeleteCtrl.js
@@ -3,6 +3,8 @@ const { responseOk } = require("../../helpers/responses");
 
 const transactionDeleteCtrl = async (req, res, next) => {
   const userId = req.user._id;
+  console.log(userId);
+  console.log(req.body);
   await transactionDelete(req.body, userId);
   res.status(200).json(responseOk("success", 200, "Transaction deleted"));
 };
--- services/transaction/transactionDelete.js
+++ services/transaction/transactionDelete.js
@@ -1,7 +1,8 @@
 const { TransactionSchema } = require("../../models/transaction");
 
 const transactionDelete = async ({ _id }, owner) => {
-  await TransactionSchema.findByIdAndDelete({ _id }, owner);
+  console.log(_id, owner);
+  await TransactionSchema.findByIdAndDelete({ _id, owner });
 };
 
 module.exports = transactionDelete;
