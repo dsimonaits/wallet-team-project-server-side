b9f91c37bf63e6b909192dd14e0538468fde5936
Soan1488 <andrisokolovskiy@gmail.com>
2023-04-16 18:32:46 +0200

    fixed cout balance

--- controllers/transaction/transactionDeleteCtrl.js
+++ controllers/transaction/transactionDeleteCtrl.js
@@ -3,9 +3,9 @@ const { responseOk } = require("../../helpers/responses");
 
 const transactionDeleteCtrl = async (req, res, next) => {
   const userId = req.user._id;
-  console.log(userId);
-  console.log(req.body);
-  await transactionDelete(req.body, userId);
+  await transactionDelete(req.body, userId).catch((e) => {
+    console.log(e);
+  });
   res.status(200).json(responseOk("success", 200, "Transaction deleted"));
 };
 
