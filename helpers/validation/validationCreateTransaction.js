ac98e18d93d31d5596c6afd1ac28259df26367ea
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-17 01:40:46 +0300

    Update validationCreateTransaction.js

--- helpers/validation/validationCreateTransaction.js
+++ helpers/validation/validationCreateTransaction.js
@@ -1,7 +1,7 @@
 const joi = require("joi");
 
 const validationCreateTransaction = joi.object({
-  type: joi.number().required(),
+  type: joi.boolean().required(),
   sum: joi.number().not(0).precision(2).required(),
   category: joi.string(),
   date: joi.date().required(),
