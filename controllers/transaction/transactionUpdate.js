45610978f0465d6e9048ceaaa5848d2dcd835468
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-21 23:38:09 +0300

    Update transactionUpdate.js

--- controllers/transaction/transactionUpdate.js
+++ controllers/transaction/transactionUpdate.js
@@ -1,4 +1,5 @@
 const { transactionUpdate } = require("../../services/transaction");
+const { responseOk } = require("../../helpers/responses");
 
 const transactionUpdateCtrl = async (req, res) => {
   const userId = req.user._id;
@@ -8,6 +9,9 @@ const transactionUpdateCtrl = async (req, res) => {
     userId
   );
   res.status(200).json(updatedTransaction);
+  res
+    .status(200)
+    .json(responseOk("success", 200, "Transaction saved", updatedTransaction));
 };
 
 module.exports = transactionUpdateCtrl;
