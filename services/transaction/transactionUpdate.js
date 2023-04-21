8af0d60cd330c5e984744a5b944f4b296c520a8c
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-21 23:32:40 +0300

    Update transactionUpdate.js

--- services/transaction/transactionUpdate.js
+++ services/transaction/transactionUpdate.js
@@ -23,8 +23,8 @@ const transactionUpdate = async (data, transactionId, userId) => {
   }
   await TransactionSchema.findByIdAndUpdate(transactionId, data);
   const newTransaction = await TransactionSchema.findById(transactionId);
-  const user = await getUserData(userId);
-  return { newTransaction, balance: user.balance };
+  const { balance } = getUserData(userId);
+  return { newTransaction, balance };
 };
 
 module.exports = transactionUpdate;
