51b5475d07362465ef17185f41e8892009488163
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-23 23:30:05 +0300

    Update loginController.js

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -15,7 +15,7 @@ const loginController = async (req, res) => {
   res.cookie("refreshToken", data.refreshToken, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: none,
+    sameSite: "none",
     secure: true,
   });
   res.json(responseOk("success", 200, "Login is successful", data));
