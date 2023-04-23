10c6d50a99746ee87436c5c0cf298fc26fb2d99c
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-23 17:06:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(checkConnection);
 app.use(logger(formatsLogger));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
