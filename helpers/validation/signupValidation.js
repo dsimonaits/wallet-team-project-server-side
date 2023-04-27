900b91a1fcd15b8ac42a70aa5e8f6b9c5c47913e
Galina Matvienko <galyagmgm@gmail.com>
2023-04-27 16:50:12 +0200

    commit
    This regular expression matches passwords that contain at least one letter (uppercase or lowercase) and at least one digit, and are at least 6 characters long. Here are some examples of passwords that would match:
    
    password1
    
    hello123

--- helpers/validation/signupValidation.js
+++ helpers/validation/signupValidation.js
@@ -12,11 +12,10 @@ const schemaValidate = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   
    password: Joi.string()
-    .regex(/^(?=.*\d)(?=.*[a-z]){6,16}$/)
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)
      
     .required(),
   email: Joi.string().email({ minDomainSegments: 2 }).required(),
 });
 
 module.exports = schemaValidate;
-// 
