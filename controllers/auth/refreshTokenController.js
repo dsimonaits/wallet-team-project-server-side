4cdf854c063589c79da7f914340cb25570945508
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-29 17:17:12 +0300

    Setting sameSite: "none" to refreshToken response

--- controllers/auth/refreshTokenController.js
+++ controllers/auth/refreshTokenController.js
@@ -4,10 +4,11 @@ const { responseOk } = require("../../helpers/responses");
 const refreshTokenController = async (req, res, next) => {
   const { refreshToken } = req.cookies;
   const data = await refreshTokenService(refreshToken);
-
   res.cookie("refreshToken", data.refreshToken, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   });
   res.json(responseOk("success", 200, "Refresh is successful", data));
 };
