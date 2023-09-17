4a0f794ee2fef8d70c208eb10606384d9b4e6005
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-18 00:54:46 +0300

    Update catchAsync.js

--- helpers/errors/catchAsync.js
+++ helpers/errors/catchAsync.js
@@ -1,14 +1,3 @@
-// const catchAsync = (controller) => (req, res, next) =>
-//   Promise.resolve(controller(req, res)).catch(next);
-
-// module.exports = catchAsync;
-
-// const catchAsync = (controller) => (req, res, next) => {
-//   controller(req, res, next).catch((error) => {
-//     controller(req, res).catch(next);
-//   });
-// };
-
 const catchAsync = (controller) => {
   return (req, res, next) => {
     controller(req, res).catch(next);
